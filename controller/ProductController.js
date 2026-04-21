859097ea196abe2066ff23e25c91e24a6ce8ee3f
Farid Anggoro <faridanggoro01@gmail.com>
2026-04-21 20:39:42 +0700

    filter produk dan pagination

--- controller/ProductController.js
+++ controller/ProductController.js
@@ -11,7 +11,36 @@ export const CreateProduct = asyncHandler(async (req, res) => {
 });
 
 export const AllProduct = asyncHandler(async (req, res) => {
-  const data = await Product.find();
+  // request query => melakukan searching
+  const queryObj = { ...req.query };
+
+  // fungsi mengabaikan jika ada req page & limit
+  const excludeField = ["page", "limit"];
+  excludeField.forEach((element) => delete queryObj[element]);
+
+  // fungsi filter
+  // ambil semua product berdasarkan query objek
+  let query = Product.find(queryObj);
+
+  // fungsi pagination
+  const page = req.query.page * 1 || 1; // mulai dari page 1
+  const limitData = req.query.limit * 1 || 30; // menampilkan data maksimal 30
+  const skipData = (page - 1) * limitData; // skip data berdasarkan page
+
+  // tambahkan fungsi query
+  query = query.skip(skipData).limit(limitData);
+
+  // kondisi
+  if (req.query.page) {
+    const numProduct = await Product.countDocuments(); // menghitung jumlah data
+    // jika page sudah melebihi jumlah data produk
+    if (skipData >= numProduct) {
+      res.status(404);
+      throw new Error("Halaman ini tidak ditemukan");
+    }
+  }
+
+  const data = await query;
 
   return res.status(200).json({
     message: "Menampilkan semua data product",
